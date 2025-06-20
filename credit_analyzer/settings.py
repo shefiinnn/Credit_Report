7d6fe7de41b555340ce314342802791e6ff26560
shefiinnn <shazzshefin@gmail.com>
2025-06-20 10:59:13 +0530

    commit final

--- credit_analyzer/settings.py
+++ credit_analyzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3_)0c37rt*ex$1)$ds=j$@o)eha6is%5tg+1*_2%upy1v@*ds9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','13.60.35.140','localhost']
 
 
 # Application definition
